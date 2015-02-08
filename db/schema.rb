e9fabecb1edce718c85a61271965099e939b1565
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-02-08 19:45:03 -0300

    add role to user and wip on abilities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208214328) do
+ActiveRecord::Schema.define(version: 20150208220813) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150208214328) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
