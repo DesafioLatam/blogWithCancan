97a5d74099be17de67c9fc5b99c0594921b0aed6
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-02-08 20:05:49 -0300

    add relations for comments and post to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208220813) do
+ActiveRecord::Schema.define(version: 20150208230402) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
