67942983aef635060e2cf67fede8c964a6b08c49
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-02-08 18:34:45 -0300

    saving comments on comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
   def create
+  	@post = Post.find(params[:post_id])
+  	@comment = @post.comments.build(comment_params)
+  	@comment.save
+  	redirect_to posts_path
+
   end
 
   def update
@@ -7,4 +12,9 @@ class CommentsController < ApplicationController
 
   def destroy
   end
+
+  def comment_params
+  	params.require(:comment).permit(:content)
+  end
+
 end
