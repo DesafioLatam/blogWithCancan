506bb4b7d2754a306eedaa96759d3b8a8680ae96
Gonzalo Sanchez <gonzalo.sanchez.d@gmail.com>
2015-02-08 23:23:38 -0300

    saving user on comments#create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
   	@post = Post.find(params[:post_id])
   	@comment = @post.comments.build(comment_params)
+    @comment.user = current_user
   	@comment.save
   	redirect_to posts_path
 
